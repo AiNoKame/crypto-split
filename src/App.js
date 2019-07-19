690fa44d00f4aae0dab33da0dbc6da11d2750db6
Rod Calumpong <Rod.Calumpong@gmail.com>
2019-07-19 14:01:45 -0700

    Add request dependency

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 import MyQrReader from './QrReader.js';
 import Button from 'react-bootstrap/Button';
+import request from 'request';
+import JsonResponse from './JsonResponse.js'
 
 var qrData = '';
+var checkOutResponse = '';
 
 function setQrDataParent(qrData) {
   qrData = qrData;
+}
+
+function handleCheckOut() {
+  request('http://www.google.com', function (error, response, body) {
+    console.error('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status
+    console.log('body:', body); // Print the HTML for the Google homepage.
+    checkOutResponse = body;
+  });
 }
 
 function App() {
@@ -20,13 +32,10 @@ function App() {
         </p>
         <MyQrReader setQrDataChild={setQrDataParent} />
         <p>{qrData}</p>
-        {
-          qrData ?
-            <Button variant="primary" size="lg">
-              CHECK OUT
-            </Button>
-            : null
-        }
+        <Button variant="primary" size="lg" onClick={handleCheckOut}>
+          CHECK OUT
+        </Button>
+        <JsonResponse jsonResponse={checkOutResponse}/>
       </header>
     </div>
   );
---
+++ src/JsonResponse.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class JsonResponse extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.jsonResponsne}
+      </div>
+    )
+  }
+}
+
+export default JsonResponse;
